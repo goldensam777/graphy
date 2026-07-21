ebb0e10f5105ab48d58bffe2231507beb8febe68
leumas-nedlog <leumas-nedlog@users.noreply.github.com>
2026-07-22 00:50:23 +0100

    feat(algorithms): implemented dijkstra first version based on architecture

--- graphy/algorihtms/dijkstra.py
+++ graphy/algorihtms/dijkstra.py
@@ -1,13 +1,21 @@
-from graphy import Digraph
+from graphy import *
+from graphy.utils.digraphfuncs import costs, relax
 
 # Write the algorithm as a function
 
-def λ(digraph: set[Digraph]):
-    return {element}
 
-
-def dijkstra(digraph: set[Digraph]):
+def dijkstra(digraph: Digraph, source: Node) -> tuple[dict[Node, float], dict[Node, Node]]:
     """
     Computes the shortest path from a single source vertex to all other vertices in a weighted digraph with non-negative weights.
     If weights are negative, Bellman-Ford must be used.
     """
+    lam = costs(digraph, source)
+    pred = {v: None for v in digraph.nodes}
+    S = set()
+    while len(S) < len(digraph.nodes):
+        i = min({v for v in digraph.nodes if v not in S}, key=lam.get)
+        S.add(i)
+        for j, arcs in digraph.adj[i].items():
+            for arc in arcs:
+                relax(lam, pred, i, j, arc.cost)
+    return lam, pred
